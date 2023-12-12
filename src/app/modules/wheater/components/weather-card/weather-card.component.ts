2ebd3459b6c939782f17239230e3b10cd109fbdf
FabrícioAndrade39 <fabricioandradegt@gmail.com>
2023-12-12 13:41:32 -0300

    Implementando o componente weather-card

--- src/app/modules/wheater/components/weather-card/weather-card.component.ts
+++ src/app/modules/wheater/components/weather-card/weather-card.component.ts
@@ -1,18 +1,20 @@
 import { WeatherDatas } from 'src/app/models/interfaces/WeatherDatas';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input} from '@angular/core';
+import { faDroplet, faTemperatureHigh, faTemperatureLow, faWind } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-weather-card',
   templateUrl: './weather-card.component.html',
   styleUrls: [],
 })
-export class WeatherCardComponent implements OnInit {
+export class WeatherCardComponent {
+  //Dados da previsão do tempo recebidos pelo componente pai
+
   @Input() weatherDatasInput!: WeatherDatas;
 
-  ngOnInit(): void {
-    console.log(
-      'DADOS RECEBIDOS DO COMPONENTE PAI - WEATHER-CARD',
-      this.weatherDatasInput
-    );
-  }
+  minTemperatureIcon = faTemperatureLow;
+  maxTemperatureIcon = faTemperatureHigh;
+  humidityIcon = faDroplet;
+  windIcon = faWind;
+
 }
